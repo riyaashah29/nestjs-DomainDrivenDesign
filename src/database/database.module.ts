6c0de16d2ac92e29cfe163dd3dfe19c006da4126
Riya <riya@gmail.com>
2023-07-21 10:12:37 +0530

    first

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('MONGO_COMMENTION')],
+  imports: [MongooseModule.forRoot('MONGO_CONNENTION')],
 })
 export class DatabaseModule {}
